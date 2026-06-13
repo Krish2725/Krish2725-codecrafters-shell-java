3ce5bef3b37af1af2b0af6c013d23db5cd4e1290
Krish6353 <krishnaisthebest25@gmail.com>
2026-06-13 19:27:09 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        int k =100;
+        while(k==100){
         Scanner in= new Scanner(System.in);
         System.out.print("$ ");
         String command =in.nextLine();
         System.out.print(command+ ": command not found");
+        System.out.println();
+        }
     }
 }
